e5d327971e955ebbdda86d0600971cad204e7e4c
EHIO <1289868863@qq.com>
2018-02-27 15:29:16 +0800

    脚本

---
+++ activity/prize.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE `activity`;
+
+USE `activity`;
+
+INSERT INTO `activity`.`t_prize` (`id`, `activity_id`, `name`, `grade`, `probability`, `upper_limit`, `type`, `amount`, `gmt_modified`, `gmt_create`) VALUES ('1', '1', '谢谢参与', '7', '78.70', '1200', '4', '0.00', '2018-02-23 10:56:51', '2018-02-23 10:56:51');
+INSERT INTO `activity`.`t_prize` (`id`, `activity_id`, `name`, `grade`, `probability`, `upper_limit`, `type`, `amount`, `gmt_modified`, `gmt_create`) VALUES ('2', '1', '3元现金', '6', '15.70', '500', '2', '3.00', '2018-02-23 10:56:51', '2018-02-23 10:56:51');
+INSERT INTO `activity`.`t_prize` (`id`, `activity_id`, `name`, `grade`, `probability`, `upper_limit`, `type`, `amount`, `gmt_modified`, `gmt_create`) VALUES ('3', '1', '5元现金', '5', '0.80', '200', '2', '5.00', '2018-02-23 10:56:51', '2018-02-23 10:56:51');
+INSERT INTO `activity`.`t_prize` (`id`, `activity_id`, `name`, `grade`, `probability`, `upper_limit`, `type`, `amount`, `gmt_modified`, `gmt_create`) VALUES ('4', '1', '10元现金', '4', '0.80', '100', '2', '10.00', '2018-02-23 10:56:51', '2018-02-23 10:56:51');
+INSERT INTO `activity`.`t_prize` (`id`, `activity_id`, `name`, `grade`, `probability`, `upper_limit`, `type`, `amount`, `gmt_modified`, `gmt_create`) VALUES ('5', '1', '香肠腊肉礼盒', '3', '1.60', '50', '0', '86.00', '2018-02-23 10:56:51', '2018-02-23 10:56:51');
+INSERT INTO `activity`.`t_prize` (`id`, `activity_id`, `name`, `grade`, `probability`, `upper_limit`, `type`, `amount`, `gmt_modified`, `gmt_create`) VALUES ('6', '1', '三只松鼠套装', '2', '1.60', '20', '0', '99.00', '2018-02-23 10:56:51', '2018-02-23 10:56:51');
+INSERT INTO `activity`.`t_prize` (`id`, `activity_id`, `name`, `grade`, `probability`, `upper_limit`, `type`, `amount`, `gmt_modified`, `gmt_create`) VALUES ('7', '1', '中粮大礼包', '1', '0.80', '10', '0', '158.00', '2018-02-23 10:56:51', '2018-02-23 10:56:51');
